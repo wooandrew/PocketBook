629e45682c75614cd386c895c4dfa0b7ea2367a6
Andrew Woo <seungminleader@gmail.com>
2026-01-07 23:12:12 -0500

    Update run script to run/build to configuration type directory

--- run.ps1
+++ run.ps1
@@ -3,42 +3,42 @@
 param(
     [Parameter(Mandatory=$true)]
     [ValidateSet("clean", "build", "run", "all")]
-    [string]$Target
+    [string]$Target,
+
+    [ValidateSet("Debug", "Release")]
+    [string]$Config = "Debug"
 )
 
-$buildDir = "build"
-$binDir = "bin/"
+$buildDir   = "build/$Config"
+$binDir     = "bin/$Config"
 $executable = "finmanp.exe"  # change if needed
 
 function Clean {
-    Write-Host "Cleaning build directory..."
-    if (Test-Path $buildDir) {
-        Remove-Item -Recurse -Force $buildDir
-    } else {
-        Write-Host "Build directory does not exist."
+    Write-Host "Cleaning build and bin directories..."
+    if (Test-Path "build") {
+        Remove-Item -Recurse -Force "build"
     }
-
-    if (Test-Path $binDir) {
-        Remove-Item -Recurse -Force $binDir
-    } else {
-        Write-Host "Bin directory does not exist."
+    if (Test-Path "bin") {
+        Remove-Item -Recurse -Force "bin"
     }
 }
 
 function Configure {
-    Write-Host "Configuring project..."
-    cmake -S . -B $buildDir
+    Write-Host "Configuring project ($Config)..."
+    cmake -S . -B $buildDir `
+        -DCMAKE_BUILD_TYPE=$Config `
+        -DCMAKE_RUNTIME_OUTPUT_DIRECTORY="$PWD/$binDir"
 }
 
 function Build {
-    Write-Host "Building project..."
-    cmake --build $buildDir
+    Write-Host "Building project ($Config)..."
+    cmake --build $buildDir --config $Config
 }
 
 function Run {
     $exePath = Join-Path $binDir $executable
     if (Test-Path $exePath) {
-        Write-Host "Running executable..."
+        Write-Host "Running executable ($Config)..."
         & $exePath
     } else {
         Write-Host "Executable not found at $exePath"
